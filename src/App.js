c5b5c26256851aafe42b1b228e4dfbcf3d4ee61a
Isaac Zepeda <keogh24@gmail.com>
2022-04-07 07:33:17 -0600

    Playing around with images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React, { useRef } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const inputRef = useRef()
+
+  const handleChange = e => {
+    console.log(e.target.files)
+
+    // One way to get image to put in img
+    console.log(URL.createObjectURL(e.target.files[0]))
+
+    // Second way to get the image this gets the base64 string
+    console.log('Ref files', inputRef.current.files)
+    const reader = new FileReader();
+    console.log('reader result', reader.result)
+    console.log(reader.readAsDataURL(e.target.files[0]))
+    console.log('reader result', reader.result)
+    reader.addEventListener('load', () => {
+      console.log('load')
+      console.log('reader result', reader.result)
+    }, false)
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <div>
+        <input ref={inputRef} type="file" multiple onChange={handleChange}/>
+      </div>
+      <div>Panel</div>
+    </div>
   );
 }
 
